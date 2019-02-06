96077b8e0f5b96f50e1cc7beab7cbfa4c7bf8631
José Ángel García Salinas <agarcia@luxoft.com>
2019-02-06 17:24:13 -0600

    adding clean up work

--- app/src/main/java/com/yobijoss/workmanagerdemo/utils/Constants.kt
+++ app/src/main/java/com/yobijoss/workmanagerdemo/utils/Constants.kt
@@ -4,5 +4,6 @@ package com.yobijoss.workmanagerdemo.utils
 //Workflow Constants
 const val KEY_COMPRESS_URI = "imageCompressKey"
 const val KEY_UPLOAD_URI = "imageUploadKey"
+const val PROCESS_IMAGE_WORK_NAME = "ProcessImageWork"
 
 const val OUTPUT_PATH = "compress_images"
